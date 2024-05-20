03bee954ef17aecd08b3035eb5f3265450cff844
SareyBoth <chansareyboth@gmail.com>
2024-05-20 14:39:07 +0700

    first python

---
+++ hello.py
@@ -0,0 +1,10 @@
+
+# Starting number
+number = 1
+
+# Loop while number is less than or equal to 5
+while number <= 5:
+  # Print the number
+  print(number)
+  # Increment the number by 1
+  number += 1
